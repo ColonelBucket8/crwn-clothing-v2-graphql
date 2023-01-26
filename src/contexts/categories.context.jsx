e302fefa553ce7603afe241244ede92801f0a783
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-26 17:45:16 +0800

    Wrap component with loading spinner when graphql is fetching

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -22,10 +22,23 @@ const COLLECTIONS = gql`
 `;
 
 export const CategoriesProvider = ({ children }) => {
-  const { loading, error, data } = useQuery(COLLECTIONS);
+  const { loading, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  const value = { categoriesMap };
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data]);
+
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
